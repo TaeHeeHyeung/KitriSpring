fe75cf07a9b190e9499401a08dedb525ae4e6af6
hth0893 <hth0893@naver.com>
2019-06-14 11:19:14 +0900

    first commit

---
+++ hellospring/src/com/kitri/hello3/HelloMain.java
@@ -0,0 +1,13 @@
+package com.kitri.hello3;
+
+public class HelloMain {
+	public static void main(String[] args) {
+		HelloService helloService =  new HelloServiceKor();
+		String msg = helloService.hello("형태희");
+		
+//		HelloServiceEng helloServiceEng =  new HelloServiceEng();
+//		String msgEng = helloServiceEng.hello("형태희");
+		
+		System.out.println(msg);	
+	}
+}	
---
+++ hellospring/src/com/kitri/hello3/HelloServiceEng.java
@@ -0,0 +1,8 @@
+package com.kitri.hello3;
+
+public class HelloServiceEng implements HelloService {
+	public String hello(String name) {
+		return "Hello "+name +"!!";
+	}
+
+}
